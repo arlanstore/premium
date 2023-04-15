44a34eba7fc81b494e437ae9e54fecb8b0b21bd7
arlanstore <122625984+arlanstore@users.noreply.github.com>
2023-04-15 23:33:50 +0700

    Add files via upload

---
+++ INSTALL/IPSEC/cek-pptp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Information owner this script
+server_owner=https://raw.githubusercontent.com/arlanstore/premium/main
+
+# Check permision access
+  IPVPS=$(curl -s ipv4.icanhazip.com)
+  IZIN=$(curl -s ${server_owner}/Autoscript/member_premium | grep ${IPVPS} | cut -d ' ' -f 1)
+  if [[ $IPVPS = $IZIN ]]; then
+  echo ""
+  else
+  clear
+  figlet Firdaus project | lolcat
+  echo ""
+  echo "   AUTOSCRIPT HAS EXPIRED"
+  echo ""
+  echo "   THIS OWNER CONTACT   > WHATSAPP : +6283805933853"
+  echo "                        > TELEGRAM : @arlanstore"
+  echo "                        > EMAIL    : arlanvpnstore@gmail.com"
+  echo ""
+  echo "   THIS CREATOR CONTACT > WHATSAPP : +6283805933853876989"
+  echo "                        > TELEGRAM : @arlanstore"
+  echo "                        > EMAIL    : arlanvpnstore@gmail.com"
+  echo ""
+  exit 1
+  fi
+
+last | grep ppp | grep still | awk '{print " ",$1," - " $3 }' > /tmp/login-db-pptp.txt;
+echo ""
+echo "           PPTP VPN User Login";
+echo "===========================================";
+echo "       Username   -------   IP";
+echo "-------------------------------------------";
+echo ""
+cat /tmp/login-db-pptp.txt
+echo ""
+echo "===========================================";
+echo ""
+echo ""
