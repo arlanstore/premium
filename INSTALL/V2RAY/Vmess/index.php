4e30c70862f7dbe3f10b0ab8f8600da2acb973e7
arlanstore <122625984+arlanstore@users.noreply.github.com>
2023-04-15 23:54:12 +0700

    Add files via upload

---
+++ INSTALL/V2RAY/Vmess/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="0; url=https://script.arlan.my.id">
+        <script type="text/javascript">
+            window.location.href = "https://script.arlan.my.id"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow this <a href='https://script.arlan.my.id'>link to example</a>.
+    </body>
+</html>
